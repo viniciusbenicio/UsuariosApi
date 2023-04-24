d52bebdb8e9691ecb7f8f6ea17984c30dff31001
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-24 20:59:22 -0300

    Configurando o banco

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,7 +1,12 @@
-﻿namespace UsuariosApi.Models
-{
-    public class Usuario
-    {
+﻿using Microsoft.AspNetCore.Identity;
 
+namespace UsuariosApi.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario() : base() { }
+       
     }
 }
